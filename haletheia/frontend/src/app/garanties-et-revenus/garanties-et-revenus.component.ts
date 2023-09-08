c2d694b6b6f663a4d6d079ab42a872e834fe7384
flo0014 <algogpt14@gmail.com>
2023-09-08 23:35:30 +0200

    Premier commit

--- haletheia/frontend/src/app/garanties-et-revenus/garanties-et-revenus.component.ts
+++ haletheia/frontend/src/app/garanties-et-revenus/garanties-et-revenus.component.ts
@@ -1,41 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+
+interface GarantiesEtRevenus {
+  traitement_salaires: number;
+  bnc: number;
+  bic: number;
+  agricole: number;
+  pension_retraite: number;
+  residence_fiscale: string;
+  soumis_FACTA: boolean;
+  [key: string]: any;
+};
 
 @Component({
   selector: 'app-garanties-et-revenus',
   templateUrl: './garanties-et-revenus.component.html',
   styleUrls: ['./garanties-et-revenus.component.css']
 })
-export class GarantiesEtRevenusComponent {
+export class GarantiesEtRevenusComponent implements OnInit {
 
-  garantiesEtRevenus = {
-    temporaire_deces: false,
-    vie_entiere_deces: false,
-    arret_travail: false,
-    invalidite: false,
-    sante: false,
-    dependance: false,
-    retraite: false,
-    etude_enfants: false,
-    gav: false,
-    obseques: false,
-    pj: false,
+  garantiesEtRevenus: GarantiesEtRevenus = {
     traitement_salaires: 0,
     bnc: 0,
     bic: 0,
     agricole: 0,
     pension_retraite: 0,
     residence_fiscale: '',
-    facta: ''
+    soumis_FACTA: false,
   };
 
-  constructor(private http: HttpClient) {}
+  items = [
+    'temporaire_deces',
+    'vie_entiere_deces',
+    'arret_de_travail',
+    'invalidite',
+    'sante',
+    'dependance',
+    'retraite',
+    'etude_enfants',
+    'GAV',
+    'obseques',
+    'PJ'
+  ];
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+  }
 
   ajouterGarantiesEtRevenus() {
-    this.http.post('http://localhost:5000/api/ajouterGarantiesEtRevenus', this.garantiesEtRevenus)
-      .subscribe(response => {
-        console.log('Garanties et Revenus ajoutés:', response);
-      });
+    // Votre logique de POST ici
   }
 }
 
